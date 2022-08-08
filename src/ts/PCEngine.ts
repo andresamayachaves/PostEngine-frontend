46b1e828d582aadeb74233852336658499f62dfd
andresamayachaves <andresamayachaves@gmail.com>
2022-08-08 11:02:38 -0500

    Worked on PCEngine.ts

--- src/ts/PCEngine.ts
+++ src/ts/PCEngine.ts
@@ -1,5 +1,5 @@
 //import { commentI } from "./models/models.js"
-import { postI } from "./models/models.js"
+import { commentI, postI } from "./models/models.js"
 import { getAllPostsFromBacked } from "./requests/asyncRequests.js" 
 
 console.log("Ts created")
@@ -19,14 +19,37 @@ function materializePosts(posts: Array<postI>){
 }
 
 function renderPost(post:postI, divRoot:HTMLDivElement){    
+    "container-post-${post.id}"
     const singlePostContainer =  document.createElement('div')
-    singlePostContainer.className = 'container-post-${post.id}'
+    singlePostContainer.className = 'single-post-container${post.id}'
     singlePostContainer.classList.add('single_post_container')
 
     const singlePostContent = `
-    <h2 class="single-post-name-${post.id}">${post.content}> </h2>
+    <h2 class="single-post-name-${post.id}">${post.content} </h2>
     <p clas="Comment-${post.id}"> ${post.comments} </p>
     `
-    singlePostContainer.innerHTML = singlePostContent    
+    singlePostContainer.innerHTML = singlePostContent  
+    materializeComments(post.comments, singlePostContainer)
+    divRoot.append(singlePostContainer)
 }
 
+function materializeComments(comments:commentI[], 
+    postContainer:HTMLDivElement){
+comments.forEach(comment => renderComment(comment, postContainer))
+}
+
+function renderComment(comment:commentI, 
+postContainer:HTMLElement){
+    const singleCommentContainer:HTMLDivElement = document.createElement('div')
+    singleCommentContainer.className = `single_comment_container_${comment.id}`
+    singleCommentContainer.classList.add('single_comment_container')
+
+    const singleCommentContent:string = `
+    <h3 class="comment_content_${comment.id}"> ${comment.content}</h3>
+    <p class="comment_numeberOfLikes_${comment.id}"> ${comment.numberOfLikes}</p>
+    <p class="comment_userLikes_${comment.id}"> ${comment.userLikes}</p>
+    `
+
+    singleCommentContainer.innerHTML = singleCommentContent;
+    postContainer.append(singleCommentContainer)
+}
